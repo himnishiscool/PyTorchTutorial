15514d1dd7c90b88fc2ac68fc73dc2091abaf9e3
Himnish Dukkola <himnishthegod@gmail.com>
2024-11-25 09:55:37 -0600

    Learned some basic commands

--- main.py
+++ main.py
@@ -1,19 +1,47 @@
 import torch
-import numpy as np
 
-my_list = [
-    [1, 2, 3, 4, 5],
-    [6, 7, 8, 9, 10]
-]
-np1 = np.random.rand(3, 4)
-tensor = torch.randn(3, 4)
-tensor_3d = torch.zeros(2, 3, 4)
+my_torch = torch.arange(10)
+print(my_torch)
 
-my_tensor = torch.tensor(np1)
+# my_torch.reshape(2, 6) # doesn't work! Intentional, the number pair doesn't equal to 10, 2 x 6 = 12, not 10
 
-print(my_list)
-print(np1)
-print(tensor)
-print(tensor_3d)
+my_torch2 = torch.arange(10)
+print(my_torch2)
 
-print(my_tensor)
+my_torch2 = my_torch2.reshape(2, -1)
+print(my_torch2)
+
+my_torch3 = torch.arange(10)
+print(my_torch3)
+
+my_torch4 = my_torch3.view(2, 5)
+print(my_torch4)
+
+# When you use reshape or view, the tensor will update
+
+my_torch5 = torch.arange(10)
+print(my_torch5)
+
+my_torch6 = my_torch5.reshape(2, 5)
+print(my_torch6)
+
+my_torch5[1] = 1234  # my_torch6 will update if we print it out after we change the value.
+print(my_torch5)
+print(my_torch6)
+
+# Slices
+my_torch7 = torch.arange(10)
+print(my_torch7)
+
+# Grab a specific item
+print(my_torch7[7])
+
+# Grab a slice
+my_torch8 = my_torch7.reshape(5, 2)
+print(my_torch8)
+
+# Returns Row
+print(my_torch8[:, 1])
+
+# Returns Column
+print(my_torch8[:, 1:])
